7aa6aaf5cedd9894beb1d26a80de4fdc1925dc83
Bernard Bussy <bernard.r.bussy@gmail.com>
2025-02-28 17:16:31 +0100

    delete node grpc clients

--- ts/src/sdk/client.ts
+++ ts/src/sdk/client.ts
@@ -1,25 +1,25 @@
 import { ServiceClient as FeeProfileServiceClient } from "../instrument/feeprofile/ServiceServiceClientPb";
 
 class InstrumentDomain {
-    private _feeProfile: FeeProfileServiceClient;
+  private _feeProfile: FeeProfileServiceClient;
 
-    constructor() {
-        this._feeProfile = new FeeProfileServiceClient("");
-    }
+  constructor() {
+    this._feeProfile = new FeeProfileServiceClient("");
+  }
 
-    public get feeProfile(): FeeProfileServiceClient {
-        return this._feeProfile;
-    }
+  public get feeProfile(): FeeProfileServiceClient {
+    return this._feeProfile;
+  }
 }
 
 export class Client {
-    private _instrument: InstrumentDomain;
+  private _instrument: InstrumentDomain;
 
-    constructor() {
-        this._instrument = new InstrumentDomain();
-    }
+  constructor() {
+    this._instrument = new InstrumentDomain();
+  }
 
-    public get instrument(): InstrumentDomain {
-        return this._instrument;
-    }
+  public get instrument(): InstrumentDomain {
+    return this._instrument;
+  }
 }
