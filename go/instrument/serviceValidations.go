7582e0784cb8e454cbbc3b7bd0c56f126c05a7a8
Bernard Bussy <bernard.r.bussy@gmail.com>
2025-03-03 09:57:09 +0100

    start instrument service validations

---
+++ go/instrument/serviceValidations.go
@@ -0,0 +1,20 @@
+package instrument
+
+import (
+	fmt "fmt"
+	"strings"
+)
+
+func (r *MintRequest) Validate() error {
+	reasons := []string{}
+
+	if r.GetFeeAccountNumber() == "" {
+		reasons = append(reasons, "feeAccountNumber not set")
+	}
+
+	if len(reasons) > 0 {
+		return fmt.Errorf("validation failed: %s ", strings.Join(reasons, "; "))
+	}
+
+	return nil
+}
