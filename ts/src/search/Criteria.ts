f439a4bc2e675fd6491744c242a16acf1d92e58f
Bernard Bussy <bernard.r.bussy@gmail.com>
2025-04-28 14:30:15 +0200

    update criteria with master merge

--- ts/src/search/Criteria.ts
+++ ts/src/search/Criteria.ts
@@ -1,19 +1,38 @@
 import { Criterion } from "./criterion_pb";
 
-// Utility Type to keep track of multiple criteria with key/value pairs
-export class Criteria {
-  criteriaMap: Record<string, Criterion> = {};
+export type CriteriaMap = Record<string, CriteriaMapField | undefined>;
 
-  constructor(criteriaMap?: Record<string, Criterion>) {
+export type ValueType = string | number | number[] | string[];
+
+export type CriteriaMapField = {
+  criterion?: Criterion,
+  value?: ValueType
+};
+
+/** 
+ Utility Type to keep track of multiple criteria with key/value pairs
+ */
+export class Criteria {
+  criteriaMap: CriteriaMap = {};
+
+  constructor(criteriaMap?: CriteriaMap) {
     if (!criteriaMap) return;
     this.criteriaMap = criteriaMap;
   }
 
-  updateCriterion(key: string, criterion: Criterion) {
-    this.criteriaMap[key] = criterion;
+  updateCriterion(key: string, criterion?: Criterion, value?: ValueType) {
+    this.criteriaMap[key] = {
+      criterion,
+      value,
+    };
   }
 
-  toCriterionList() {
-    return Object.keys(this.criteriaMap).map((key) => this.criteriaMap[key]);
+  getCriterionValue(key: string): ValueType | undefined {
+    return this.criteriaMap[key]?.value
+  }
+
+  toCriterionList(): Criterion[] {
+    return Object.keys(this.criteriaMap)
+      .map((key) => this.criteriaMap[key]?.criterion).filter((v) => v !== undefined);
   }
 }
