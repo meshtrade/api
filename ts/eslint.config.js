b6b06ec5cc0c61d2f8bb225ed41aa3c8d57550c0
Bernard Bussy <bernard.r.bussy@gmail.com>
2025-06-27 10:57:26 +0200

    fix linting

--- ts/eslint.config.js
+++ ts/eslint.config.js
@@ -5,6 +5,14 @@ const prettier = require('eslint-plugin-prettier/recommended');
 module.exports = tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ["*.js"],
+    languageOptions: {
+      globals: {
+        module: "readonly",
+      }
+    }
+  },
   prettier,
   {
     ignores: ["dist/**", ".prettierrc.js", "eslint.config.js", "src/**/*.d.ts", "src/**/*.js"],
