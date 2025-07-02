072e4e7a1d19a648d3825fef50744f78247778c1
lawrencemardon <lawrence@meshtrade.co>
2025-07-02 10:04:16 +0200

    proto cleanup

---
+++ ts/dist/banking/funding/standardAccountLabels_pb.d.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+// @ts-nocheck
+// package: api.banking.funding
+// file: api/proto/banking/funding/standardAccountLabels.proto
+
+/* tslint:disable */
+/* eslint-disable */
+
+import * as jspb from "google-protobuf";
+
+export enum StandardPeachAccountLabel {
+    ACCOUNT_LABEL_UNSPECIFIED = 0,
+    PEACH_FEE_ACCOUNT_STANDARD_LABEL = 1,
+    PEACH_SETTLEMENT_ACCOUNT_STANDARD_LABEL = 2,
+    PEACH_FLOAT_ACCOUNT_STANDARD_LABEL = 3,
+}
